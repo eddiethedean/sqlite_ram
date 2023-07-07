7c590ef62c5976e204690b8d5ffb484cedb70726
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2023-07-07 12:17:26 -0400

    fix ram_connection

--- sqlite_ram/backup.py
+++ sqlite_ram/backup.py
@@ -1,10 +1,15 @@
 import sqlite3
 from sqlite3 import Connection
-from typing import Optional
+from typing import Optional, Tuple
 from sqlite_utils import Database
 
 
-def ram_connection(disk_connection: Connection, name: Optional[str]) -> Connection:
-    db = Database(memory_name=name)
-    disk_connection.backup(db)
-    return db
+def ram_connection(disk_path: str, name: Optional[str] = None) -> Tuple[Database, Database]:
+    if name:
+        ram_db = Database(memory_name=name)
+    else:
+        ram_db = Database(memory=True)
+    disk_db = Database(disk_path)
+    disk_connection = sqlite3.connect(disk_path)
+    disk_connection.backup(ram_db.conn)
+    return ram_db, disk_db
